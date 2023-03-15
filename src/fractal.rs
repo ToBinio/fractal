dfa09aebce401b29a039cb03b8aa4c45925f1c0e
to_binio <tobias.frischmann1@gmail.com>
2023-03-15 12:45:36 +0100

    basic testing setup

--- src/fractal.rs
+++ src/fractal.rs
@@ -1,23 +1,19 @@
 use std::ops::Range;
 use std::sync::mpsc;
-use std::sync::mpsc::{Receiver, TryRecvError};
+use std::sync::mpsc::Receiver;
 use std::thread;
 
+use crate::util::compute_img;
 use ggez::glam::Vec2;
 use ggez::graphics::{
     Canvas, Color, DrawMode, DrawParam, Image, ImageFormat, Mesh, Rect, StrokeOptions,
 };
 use ggez::mint::Vector2;
 use ggez::Context;
-use image::{DynamicImage, EncodableLayout, GenericImage, Rgba};
-use palette::{Gradient, LinSrgb};
+use image::{DynamicImage, EncodableLayout};
 
-const SIZE: f64 = 500.0;
-
-const IMG_SIZE: u32 = 250;
-
-const MAX_ITER: u32 = 500;
-const MULTISAMPLE_SIZE: u32 = 2;
+pub const SIZE: f64 = 500.0;
+pub const IMG_SIZE: u32 = 250;
 
 pub struct FractalNode {
     complex_const: (f64, f64),
@@ -197,83 +193,7 @@ impl FractalNode {
                 let complex_const = self.complex_const;
 
                 thread::spawn(move || {
-                    let mut image = DynamicImage::new_rgb8(IMG_SIZE, IMG_SIZE);
-
-                    let gradient = Gradient::new(vec![
-                        LinSrgb::new(0.0, 0.0, 0.0),
-                        LinSrgb::new(0.0, 0.0, 1.0),
-                        LinSrgb::new(1.0, 0.0, 1.0),
-                        LinSrgb::new(1.0, 0.0, 0.0),
-                    ]);
-
-                    // every pixel
-                    for x in 0..IMG_SIZE {
-                        for y in 0..IMG_SIZE {
-                            let mut iter_sum = 0;
-
-                            //multi sampling
-                            for x_offset in 0..MULTISAMPLE_SIZE {
-                                for y_offset in 0..MULTISAMPLE_SIZE {
-                                    let x_offset = x_offset as f64 / MULTISAMPLE_SIZE as f64;
-                                    let y_offset = y_offset as f64 / MULTISAMPLE_SIZE as f64;
-
-                                    let x = x as f64 + x_offset;
-                                    let y = y as f64 + y_offset;
-
-                                    let mut iter_count = MAX_ITER;
-
-                                    let mut normal = ((x / IMG_SIZE as f64)
-                                        * (x_range.end - x_range.start).abs()
-                                        + x_range.start)
-                                        / SIZE
-                                        * 2.0;
-
-                                    let mut imaginary = ((y / IMG_SIZE as f64)
-                                        * (y_range.end - y_range.start).abs()
-                                        + y_range.start)
-                                        / SIZE
-                                        * 2.0;
-
-                                    for i in 0..MAX_ITER {
-                                        let mut temp_normal = normal.powi(2);
-                                        let mut temp_imaginary = normal * imaginary * 2.0;
-                                        temp_normal += -imaginary.powi(2);
-
-                                        temp_normal += complex_const.0;
-                                        temp_imaginary += complex_const.1;
-
-                                        normal = temp_normal;
-                                        imaginary = temp_imaginary;
-
-                                        let size = normal * normal + imaginary * imaginary;
-                                        if size > 4.0 {
-                                            iter_count = i;
-                                            break;
-                                        }
-                                    }
-
-                                    iter_sum += iter_count;
-                                }
-                            }
-
-                            let iter_avg = iter_sum as f64 / MULTISAMPLE_SIZE.pow(2) as f64;
-                            let color = gradient.get(iter_avg / MAX_ITER as f64);
-
-                            DynamicImage::put_pixel(
-                                &mut image,
-                                x,
-                                y,
-                                Rgba([
-                                    (color.red * 255.0) as u8,
-                                    (color.green * 255.0) as u8,
-                                    (color.blue * 255.0) as u8,
-                                    255,
-                                ]),
-                            );
-                        }
-                    }
-
-                    if let Ok(_) = tx.send(image) {}
+                    tx.send(compute_img(x_range, y_range, complex_const))
                 });
             }
 
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::time::Instant;
 use ggez::conf::WindowMode;
 use ggez::event::MouseButton;
 use ggez::graphics::{Color, DrawParam, Rect, Sampler, Text, TextFragment};
@@ -10,8 +11,10 @@ use ggez::{
 };
 
 use crate::fractal::{Debug, FractalNode};
+use crate::util::compute_img;
 
 mod fractal;
+mod util;
 
 struct MainState {
     fractal_node: FractalNode,
@@ -192,14 +195,24 @@ impl event::EventHandler<GameError> for MainState {
 }
 
 pub fn main() -> GameResult {
-    let (mut ctx, event_loop) = ggez::ContextBuilder::new("super_simple", "To_Binio")
-        .window_mode(
-            WindowMode::default()
-                .dimensions(1280.0, 720.0)
-                .resizable(true),
-        )
-        .build()?;
+    // let (mut ctx, event_loop) = ggez::ContextBuilder::new("super_simple", "To_Binio")
+    //     .window_mode(
+    //         WindowMode::default()
+    //             .dimensions(1280.0, 720.0)
+    //             .resizable(true),
+    //     )
+    //     .build()?;
+    //
+    // let state = MainState::new(&mut ctx)?;
+    // event::run(ctx, event_loop, state)
 
-    let state = MainState::new(&mut ctx)?;
-    event::run(ctx, event_loop, state)
+    let instant = Instant::now();
+
+    for _ in 0..100 {
+        compute_img(-2.0..2.0, -2.0..2.0, (-0.5, 0.3));
+    }
+
+    println!("{:?}", instant.elapsed());
+
+    Ok(())
 }
