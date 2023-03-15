eff6b66fdc7bf05b070ec0c05de879bea4e2b0e8
to_binio <tobias.frischmann1@gmail.com>
2023-03-15 12:07:02 +0100

    add multisampling

--- src/fractal.rs
+++ src/fractal.rs
@@ -17,6 +17,7 @@ const SIZE: f64 = 500.0;
 const IMG_SIZE: u32 = 250;
 
 const MAX_ITER: u32 = 500;
+const MULTISAMPLE_SIZE: u32 = 2;
 
 pub struct FractalNode {
     complex_const: (f64, f64),
@@ -205,51 +206,70 @@ impl FractalNode {
                         LinSrgb::new(1.0, 0.0, 0.0),
                     ]);
 
+                    // every pixel
                     for x in 0..IMG_SIZE {
-                        'outer: for y in 0..IMG_SIZE {
-                            let mut normal = ((x as f64 / IMG_SIZE as f64)
-                                * (x_range.end - x_range.start).abs()
-                                + x_range.start)
-                                / SIZE
-                                * 2.0;
+                        for y in 0..IMG_SIZE {
+                            let mut iter_sum = 0;
 
-                            let mut imaginary = ((y as f64 / IMG_SIZE as f64)
-                                * (y_range.end - y_range.start).abs()
-                                + y_range.start)
-                                / SIZE
-                                * 2.0;
+                            //multi sampling
+                            for x_offset in 0..MULTISAMPLE_SIZE {
+                                for y_offset in 0..MULTISAMPLE_SIZE {
+                                    let x_offset = x_offset as f64 / MULTISAMPLE_SIZE as f64;
+                                    let y_offset = y_offset as f64 / MULTISAMPLE_SIZE as f64;
 
-                            for i in 0..MAX_ITER {
-                                let mut temp_normal = normal.powi(2);
-                                let mut temp_imaginary = normal * imaginary * 2.0;
-                                temp_normal += -imaginary.powi(2);
+                                    let x = x as f64 + x_offset;
+                                    let y = y as f64 + y_offset;
 
-                                temp_normal += complex_const.0;
-                                temp_imaginary += complex_const.1;
+                                    let mut iter_count = MAX_ITER;
 
-                                normal = temp_normal;
-                                imaginary = temp_imaginary;
+                                    let mut normal = ((x / IMG_SIZE as f64)
+                                        * (x_range.end - x_range.start).abs()
+                                        + x_range.start)
+                                        / SIZE
+                                        * 2.0;
 
-                                let z = normal * normal + imaginary * imaginary;
-                                if z > 4.0 {
-                                    let color = gradient.get(i as f64 / MAX_ITER as f64);
+                                    let mut imaginary = ((y / IMG_SIZE as f64)
+                                        * (y_range.end - y_range.start).abs()
+                                        + y_range.start)
+                                        / SIZE
+                                        * 2.0;
 
-                                    DynamicImage::put_pixel(
-                                        &mut image,
-                                        x,
-                                        y,
-                                        Rgba([
-                                            (color.red * 255.0) as u8,
-                                            (color.green * 255.0) as u8,
-                                            (color.blue * 255.0) as u8,
-                                            255,
-                                        ]),
-                                    );
-                                    continue 'outer;
+                                    for i in 0..MAX_ITER {
+                                        let mut temp_normal = normal.powi(2);
+                                        let mut temp_imaginary = normal * imaginary * 2.0;
+                                        temp_normal += -imaginary.powi(2);
+
+                                        temp_normal += complex_const.0;
+                                        temp_imaginary += complex_const.1;
+
+                                        normal = temp_normal;
+                                        imaginary = temp_imaginary;
+
+                                        let size = normal * normal + imaginary * imaginary;
+                                        if size > 4.0 {
+                                            iter_count = i;
+                                            break;
+                                        }
+                                    }
+
+                                    iter_sum += iter_count;
                                 }
                             }
 
-                            image.put_pixel(x, y, Rgba([255, 255, 255, 255]));
+                            let iter_avg = iter_sum as f64 / MULTISAMPLE_SIZE.pow(2) as f64;
+                            let color = gradient.get(iter_avg / MAX_ITER as f64);
+
+                            DynamicImage::put_pixel(
+                                &mut image,
+                                x,
+                                y,
+                                Rgba([
+                                    (color.red * 255.0) as u8,
+                                    (color.green * 255.0) as u8,
+                                    (color.blue * 255.0) as u8,
+                                    255,
+                                ]),
+                            );
                         }
                     }
 
